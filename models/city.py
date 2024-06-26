e9a2f261b8dc049bc24b0e3322ef750185414ccf
Simangele <144337986+Simangeletutubala@users.noreply.github.com>
2024-06-27 00:19:04 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
-
+""" City Module for HBNB project """
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Class for managing city objects"""
-
+    """ The city class, contains state ID and name """
     state_id = ""
     name = ""
