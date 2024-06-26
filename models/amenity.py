755d97b6feb52b29e5c609c42499d3480b190392
Simangele <144337986+Simangeletutubala@users.noreply.github.com>
2024-06-27 00:18:12 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
-"""Module base_model
-
-This Module contains a definition for Amenity Class
-"""
-
+""" State Module for HBNB project """
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """A class that represents a amenity
-
-    Attribute:
-        name (str): the name of the amenity
-    """
-
     name = ""
